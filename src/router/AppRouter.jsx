51eed1f3ed2df0c0e3a51e40b022128d7c42ef87
Matias Calin <maticalin@gmail.com>
2023-01-04 21:21:08 -0300

    Arme directorio y deje componente para armar rutas protegidas

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ErrorScreen } from '../404/pages/ErrorScreen'
+import { AdminScreen } from '../admin/pages/AdminScreen'
+import { LoginScreen } from '../auth/pages/LoginScreen'
+import { RegisterScreen } from '../auth/pages/RegisterScreen'
+import { ProtectedRoute } from '../components/ProtectedRoute'
+import { HomeScreen } from '../user/pages/HomeScreen'
+import { MenuScreen } from '../user/pages/MenuScreen'
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path='/login' element={<LoginScreen/>} />
+            <Route path='/register' element={<RegisterScreen/>} />
+            <Route element={<ProtectedRoute/>}>
+                <Route path='/home' element={<HomeScreen/>} />
+                <Route path='/menu' element={<MenuScreen/>} />
+            </Route>
+            <Route path='/administration' element={<AdminScreen/>} />
+            <Route path='/*' element={<ErrorScreen/>} />
+            
+        </Routes>
+    </BrowserRouter>
+  )
+}
