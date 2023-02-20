d11d004880f974b878b460b417f8ac8f29519a5e
Matias Calin <maticalin@gmail.com>
2023-02-20 18:01:21 -0300

    navBar fijo

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import { Nav } from "react-bootstrap";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import "./css/navBar.css";
 
 export const Navbar = () => {
   const navigate = useNavigate();
@@ -11,12 +12,18 @@ export const Navbar = () => {
   };
 
   return (
-    <Nav className="navbar navbar-expand-sm navbar-dark bg-dark justify-content-around w-100">
-      <div className="d-flex ">
-        <Link className="navbar-brand" to="/home">
-          logo
-        </Link>
+    
+    <Nav className="navbar navbar-expand-sm navbar-dark  ">
+      
+      <div className="navBar d-flex bg-dark w-100 justify-content-around ">
         <div className="navbar-collapse">
+          <Link className="navbar-brand" to="/home">
+            <img
+              src="https://png.pngtree.com/png-vector/20220727/ourmid/pngtree-food-logo-png-image_6089719.png"
+              alt=""
+              className="mx-2 w-25 "
+            />
+          </Link>
           <div className="navbar-nav ">
             <NavLink
               className={({ isActive }) =>
@@ -56,7 +63,7 @@ export const Navbar = () => {
         <div className="navbar-collapse collapse  order-3 dual-collapse2">
           <ul className="navbar-nav ml-auto">
             <button onClick={onLogout} className="nav-item nav-link btn">
-              logout
+              Logout
             </button>
           </ul>
         </div>
