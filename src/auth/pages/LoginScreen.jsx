939963fc37cc378ea2b244280af6ffb9d87cd716
Matias Calin <maticalin@gmail.com>
2023-03-07 17:00:07 -0300

    agregue carpeta de api para conectar con el back y finalice vinculacion de login

--- src/auth/pages/LoginScreen.jsx
+++ src/auth/pages/LoginScreen.jsx
@@ -4,48 +4,76 @@ import Form from "react-bootstrap/Form";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import "./css/login.css";
 import Alert from "react-bootstrap/Alert";
+import menuApi from "../../api/menuApi";
 
 export const LoginScreen = () => {
-  const [userLog, setUserLog] = useState({
-    email: "",
-    contraseña: "",
-  });
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [msgError, setMsgError] = useState("");
+
+  const startLogin = async(email, password) =>{
+    try {
+      const resp = await menuApi.post("/auth/",{
+        email,
+        password,
+      });
+
+      if(resp.data.rol === "usuario") {
+        navigate("/home")
+      } else {
+        navigate("/administration")
+      };
+
+      
+    } catch ({response}) {
+      setError(true);
+      setMsgError(response.data.msg)
+      setTimeout(() =>{
+        setError(false);
+      }, 4000)
+      
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //inicioValidaciones
+    if (email.trim() === "") {
+      setErrorEmail(true);
+      setTimeout(() => {
+        setErrorEmail(false);
+      }, 4000);
+    } else if (password.trim() === "") {
+      setErrorContraseña(true);
+      setTimeout(() => {
+        setErrorContraseña(false);
+      }, 4000);
+    } else if (email.match(/([a-z]\w+@[a-z]+\.[a-z]{2,5})/)) {
+      
+    } 
+    //finValidaciones
+
+    startLogin(email, password);
+  }
+  
   const navigate = useNavigate();
 
   const [errorEmail, setErrorEmail] = useState(false);
   const [errorContraseña, setErrorContraseña] = useState(false);
 
-  const inputChange = (e) => {
-    setUserLog({
-      ...userLog,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const onLoginSubmit = (e) => {
-    e.preventDefault();
-    if (userLog.email.trim() === "") {
-      setErrorEmail(true);
-      setTimeout(() => {
-        setErrorEmail(false);
-      }, 4000);
-    } else if (userLog.contraseña.trim() === "") {
-      setErrorContraseña(true);
-      setTimeout(() => {
-        setErrorContraseña(false);
-      }, 4000);
-    } else if (userLog.email.match(/([a-z]\w+@[a-z]+\.[a-z]{2,5})/)) {
-      navigate("/home");
-    }
-  };
+ 
 
   return (
-    <div className="">
+    <div >
       <section className="loginSection">
         <div className="text-center mt-5 py-1">
           <h1 className="text-light">¡Bienvenidos comensales!</h1>
         </div>
+
+        {error ? <div className="errorStyle"><h3 >{msgError}</h3></div> : ""}
 
         <div className="d-flex align-items-center justify-content-center  text-light formulario-login">
           <img
@@ -53,15 +81,15 @@ export const LoginScreen = () => {
             alt=""
             className="w-25 h-25"
           />
-          <Form onSubmit={onLoginSubmit} className="px-3 my-1 ">
+          <Form onSubmit={handleSubmit} className="px-3 my-1 ">
             <Form.Group className="mb-2" controlId="formBasicEmail">
               <Form.Label>Correo electrónico</Form.Label>
               <Form.Control
                 type="email"
                 placeholder="Ingrese su email"
                 name="email"
-                value={userLog.email}
-                onChange={inputChange}
+                value={email}
+                onChange={(e) => setEmail (e.target.value)}
               />
               <Form.Text className="text-muted">
                 Recuerde que debe ser el mismo con el que se registró.
@@ -79,8 +107,8 @@ export const LoginScreen = () => {
                 type="password"
                 placeholder="Ingrese su contraseña"
                 name="contraseña"
-                value={userLog.contraseña}
-                onChange={inputChange}
+                value={password}
+                onChange={(e) => setPassword (e.target.value)}
                 // required
               />
               {errorContraseña ? (
