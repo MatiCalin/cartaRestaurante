5e27bd1f36e0e8a3056b7fbb4e3a3c296cc152ed
victoriaserranoh <victoriaserranoharo@hotmail.com>
2023-03-10 20:20:49 -0300

    home update

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from "react-redux";
-import store from "./Store/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </Provider>
   
 );
--- src/user/pages/HomeScreen.jsx
+++ src/user/pages/HomeScreen.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { CarouselHome } from '../../components/CarouselHome';
 import { CardOne } from '../../components/CardOne';
-import { AllFoods } from '../../components/AllFoods';
 import "../pages/HomeScreen.css";
 
 export const HomeScreen = () => {
@@ -9,7 +8,6 @@ export const HomeScreen = () => {
     <>
     <div>
      <CarouselHome/>
-     <AllFoods/> 
      <CardOne/> 
     </div>
     </>
