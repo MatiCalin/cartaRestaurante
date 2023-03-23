066817b20825bd25485256b67974d6de6fa12044
victoriaserranoh <victoriaserranoharo@hotmail.com>
2023-03-22 23:14:37 -0300

    home subido un quienes somos

--- src/components/CardTwo.jsx
+++ src/components/CardTwo.jsx
@@ -17,35 +17,35 @@ export const CardTwo = () => {
           title: 'Combo Big Burguer',
           text: 'Hamburguesa con doble carne, doble chedar, lechuga, tomate. Salsa a eleccion. Papas y bebida.',
           text2: '$1800',
-          button: 'comprar',
+          button: 'Comprar',
         },
         {
           img: 'https://todoenuno.app/wp-content/uploads/2022/04/Hamburguesas-2.jpg',
           title: 'Hamburguesa Simple',
           text: 'Hamburguesa simple de carne, lechuga, tomate, queso y salsa barbacoa.',
           text2: '$1000',
-          button: 'comprar',
+          button: 'Comprar',
         },
         {
           img: 'http://d3lp4xedbqa8a5.cloudfront.net/s3/digital-cougar-assets/GourmetTraveller/2013/05/08/4166/0511GT-TO-pizza-caprese-628.jpg?width=768&height=639&mode=crop&quality=75&anchor=middlecenter',
           title: 'Pizza Caprese 6 Porciones',
           text: 'Pizza con salsa de tomate, queso, rodajas de tomate cherry y albahaca.',
           text2: '$1200',
-          button: 'comprar',
+          button: 'Comprar',
         },
         {
           img: 'https://morixe.com.ar/files/recetas/thumbs/papascheddarmorixe03.jpg',
-          title: 'Papas Fritas',
+          title: 'Papas Grill',
           text: 'Porcion mediana de papas fritas con bacon y queso chedar.',
           text2: '$900',
-          button: 'comprar',
+          button: 'Comprar',
         },
         {
           img: 'https://st3.depositphotos.com/3028293/15315/i/600/depositphotos_153159806-stock-photo-chicken-nuggets-and-french-fries.jpg',
           title: 'Nuggets de Pollo',
           text: 'Porcion de 10 nuggets de pollo, con dos salsas a eleccion y papas fritas.',
           text2: '$1200',
-          button: 'comprar',
+          button: 'Comprar',
         },
     ]
 
@@ -55,10 +55,10 @@ export const CardTwo = () => {
             <h2 className='lines-effect' > PROMOCIONES DEL MES </h2>
         </div>
 
-        <div className='cardtwo-container'>
+        <div >
           <Row xs={1} md={3} className="g-4" style={{ margin: '5rem' }}>
             {array.map(( food ) => (
-              <Col>
+              <Col className='cardtwo-container'>
                 <Card className='card-food border-light secondary' bg='dark'>
                   <Card.Img className='cardimage flex-shrink-0 img-fluid rounded' variant="top" src= {food.img}/>
                   <Card.Body className='card-body'>
