ae35d6f3205af7dfe404d1394ba1e146b4d9bced
Brenda Lescano <brendaelianalescano@gmail.com>
2023-03-25 19:42:23 -0300

    agregada peticion POST al backend con axios en registerscreen

--- src/user/routes/UserRoutes.jsx
+++ src/user/routes/UserRoutes.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Footer } from "../../ui/components/Footer";
-import {  NavbarC } from "../../ui/components/NavBar";
+import { NavbarC } from "../../ui/components/NavBar";
 import { HomeScreen } from "../pages/HomeScreen";
 import { MenuScreen } from "../pages/MenuScreen";
+import { ErrorScreen } from "../../error 404/pages/ErrorScreen"
 
 export const UserRoutes = () => {
-
   const [allProducts, setAllProducts] = useState([]);
   const [total, setTotal] = useState(0);
-	const [countProducts, setCountProducts] = useState(0);
+  const [countProducts, setCountProducts] = useState(0);
 
   return (
     <div>
-      <NavbarC 
+      <NavbarC
         allProducts={allProducts}
         setAllProducts={setAllProducts}
         total={total}
@@ -22,29 +22,33 @@ export const UserRoutes = () => {
         setCountProducts={setCountProducts}
       />
       <Routes>
-<<<<<<< HEAD
-        <Route path="/home" element={<HomeScreen />} />
-        <Route path="/menu" element={<MenuScreen />} />
-        
-=======
-        <Route path="/home" element={<HomeScreen 
-          allProducts={allProducts}
-          setAllProducts={setAllProducts}
-          total={total}
-          setTotal={setTotal}
-          countProducts={countProducts}
-          setCountProducts={setCountProducts}
-        />} />
-        <Route path="/menu" element={<MenuScreen 
-          allProducts={allProducts}
-          setAllProducts={setAllProducts}
-          total={total}
-          setTotal={setTotal}
-          countProducts={countProducts}
-          setCountProducts={setCountProducts}
-        />} />
+        <Route
+          path="/home"
+          element={
+            <HomeScreen
+              allProducts={allProducts}
+              setAllProducts={setAllProducts}
+              total={total}
+              setTotal={setTotal}
+              countProducts={countProducts}
+              setCountProducts={setCountProducts}
+            />
+          }
+        />
+        <Route
+          path="/menu"
+          element={
+            <MenuScreen
+              allProducts={allProducts}
+              setAllProducts={setAllProducts}
+              total={total}
+              setTotal={setTotal}
+              countProducts={countProducts}
+              setCountProducts={setCountProducts}
+            />
+          }
+        />
         <Route path="/*" element={<ErrorScreen />} />
->>>>>>> b8d66333f89f62a45886e703ccb7bc084cbfd9b1
       </Routes>
       <Footer />
     </div>
