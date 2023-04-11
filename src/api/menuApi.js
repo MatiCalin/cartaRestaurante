b6849fec470384be3141501dfd50a49fbade6383
Matias Calin <maticalin@gmail.com>
2023-04-11 12:13:45 -0300

    conectamos ruta de backend

--- src/api/menuApi.js
+++ src/api/menuApi.js
@@ -2,7 +2,7 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import axios from "axios";
 
 const menuApi = axios.create({
-    baseURL: "http://localhost:4003/",
+    baseURL: "https://carta-restaurant-backend.vercel.app",
 });
 
 menuApi.interceptors.request.use( (config) =>{
